c644b7f447c3dbce88fb546ac4b55b9101433ccb
Marcio Frayze <mfdavid@gmail.com>
2022-02-10 10:49:52 -0300

    Migrando para a netlify + google analytics

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 rm -rf book
 mdbook build
 cp -r book/* build/
-echo "Adicionando tag do matomo"
-sed -i "s#</head>#<script type='text/javascript'>var _paq = window._paq = window._paq || []; _paq.push(['trackPageView']); _paq.push(['enableLinkTracking']); (function() { var u='https://elm.matomo.cloud/'; _paq.push(['setTrackerUrl', u+'matomo.php']); _paq.push(['setSiteId', '1']); var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/elm.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s); })(); </script></head>#g" build/*.html
+echo "Adicionando tag do Google Analytics dentro da head de todas as páginas HTML"
+sed -i "s#</head>#<script async src="https://www.googletagmanager.com/gtag/js?id=G-E1ET4YFLQM"> </script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-E1ET4YFLQM'); </script> </head>#g" build/*.html
 mkdir -p build/resources
 cp -r resources/* build/resources
 rm -rf book
